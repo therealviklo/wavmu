979facd798c1aee8d5f2037444dee475b3ec7bad
viklo <viggo@gstromberg.nu>
2021-05-20 18:03:06 +0200

    kan lägga till noter

--- sectionview.cpp
+++ sectionview.cpp
@@ -3,6 +3,23 @@
 constexpr float noteH = 17.0f;
 constexpr float noteW = 8.0f * noteH;
 
+SectionView::NoteSelect::NoteSelect() noexcept :
+	selecting(false),
+	tone(0),
+	start(0.0),
+	end(0.0) {}
+	
+Tone SectionView::NoteSelect::getTone(float y, Offset scroll)
+{
+	return 256 - (y - scroll.y) / noteH;
+}
+
+double SectionView::NoteSelect::getTime(float x, Offset scroll, TimeSignature timesig)
+{
+	const float qnoteW = noteW / timesig.btm;
+	return std::roundf((x - scroll.x) / qnoteW) * qnoteW / noteW;
+}
+
 void SectionView::captureScroll(RECT size)
 {
 	constexpr float pianoRollH = noteH * 256.0f;
@@ -67,14 +84,8 @@ LRESULT SectionView::wndProc(MainWindow& mw, UINT msg, WPARAM wParam, LPARAM lPa
 					3.0f
 				);
 			}
-			for (const auto& note : sect.section->notes)
-			{
-				const Placement r = Placement{
-					(float)note.timestamp * noteW,
-					(255 - note.tone) * noteH,
-					(float)note.length * noteW,
-					noteH
-				} + scroll;
+
+			auto drawNote = [&](const Placement& r) -> void {
 				mw.rt.drawRectangle(
 					r,
 					blue
@@ -84,6 +95,24 @@ LRESULT SectionView::wndProc(MainWindow& mw, UINT msg, WPARAM wParam, LPARAM lPa
 					darkblue,
 					1.0f
 				);
+			};
+			for (const auto& note : sect.section->notes)
+			{
+				drawNote(Placement{
+					(float)note.timestamp * noteW,
+					(255 - note.tone) * noteH,
+					(float)note.length * noteW,
+					noteH
+				} + scroll);
+			}
+			if (ns.selecting)
+			{
+				drawNote(Placement{
+					(float)ns.start * noteW,
+					(255 - ns.tone) * noteH,
+					(float)(ns.end - ns.start) * noteW,
+					noteH
+				} + scroll);
 			}
 
 			if (mw.rt.endDraw(mw.d2dfac))
@@ -128,6 +157,52 @@ LRESULT SectionView::wndProc(MainWindow& mw, UINT msg, WPARAM wParam, LPARAM lPa
 				{
 					mw.vpop();
 				}
+				return 0;
+			}
+		}
+		break;
+		case WM_RBUTTONDOWN:
+		{
+			const int x = GET_X_LPARAM(lParam); 
+			const int y = GET_Y_LPARAM(lParam); 
+			ns.tone = ns.getTone(y, scroll);
+			ns.start = ns.getTime(x, scroll, sect.section->timesig);
+			ns.end = ns.getTime(x, scroll, sect.section->timesig);
+			ns.selecting = true;
+			InvalidateRect(mw, nullptr, FALSE);
+		}
+		return 0;
+		case WM_RBUTTONUP:
+		{
+			if (ns.selecting)
+			{
+				ns.selecting = false;
+				const std::lock_guard lg(mw.tracks.mtx);
+				if (!mw.tracks.playing.test(std::memory_order_relaxed))
+				{
+					if (std::abs(ns.end - ns.start) >= 0.5 / sect.section->timesig.btm)
+					{
+						if (ns.end < ns.start)
+						{
+							sect.section->notes.emplace_back(Note{ns.end, ns.start - ns.end, ns.tone});
+						}
+						else
+						{
+							sect.section->notes.emplace_back(Note{ns.start, ns.end - ns.start, ns.tone});
+						}
+					}
+				}
+				InvalidateRect(mw, nullptr, FALSE);
+			}
+		}
+		return 0;
+		case WM_MOUSEMOVE:
+		{
+			if (ns.selecting)
+			{
+				const int x = GET_X_LPARAM(lParam);
+				ns.end = ns.getTime(x, scroll, sect.section->timesig);
+				InvalidateRect(mw, nullptr, FALSE);
 				return 0;
 			}
 		}
--- sectionview.h
+++ sectionview.h
@@ -6,6 +6,18 @@ class SectionView : public View
 private:
 	SectionRef& sect;
 	Offset scroll;
+	struct NoteSelect
+	{
+		bool selecting;
+		Tone tone;
+		double start;
+		double end;
+
+		NoteSelect() noexcept;
+
+		static Tone getTone(float y, Offset scroll);
+		static double getTime(float x, Offset scroll, TimeSignature timesig);
+	} ns;
 
 	void captureScroll(RECT size);
 public:
